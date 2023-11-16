59707753c381af1805a0c9153aff14351d941577
andre <andremendesfelix@gmail.com>
2023-11-16 10:24:34 -0300

    Introduction to Spring Data Redis - https://www.baeldung.com/spring-data-redis-tutorial

--- src/main/java/com/example/redisspring/config/RedisConfiguration.java
+++ src/main/java/com/example/redisspring/config/RedisConfiguration.java
@@ -10,8 +10,8 @@ public class RedisConfiguration {
     JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory jedisConFactory
                 = new JedisConnectionFactory();
-        jedisConFactory.setHostName("localhost");
-        jedisConFactory.setPort(6379);
+//        jedisConFactory.setHostName("localhost");
+//        jedisConFactory.setPort(6379);
         return jedisConFactory;
     }
 
